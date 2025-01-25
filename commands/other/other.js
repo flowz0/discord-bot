6defd8f7bba9523928d4825cc0cd57b3d7c29e87
flowz0 <billylflowers@gmail.com>
2025-01-24 18:49:34 -0800

    Added option and parsed input to show in reply message

--- commands/other/other.js
+++ commands/other/other.js
@@ -3,10 +3,16 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("other")
-    .setDescription("Other command"),
+    .setDescription("Other command")
+    .addStringOption(option =>
+      option.setName('input')
+        .setDescription('The input to echo back')
+    ),
   async execute(interaction) {
+    const response = interaction.options.getString('input');
+
     await interaction.reply(
-      `This is other command was run by ${interaction.user.username}.`
+      `This is other command was run by ${interaction.user.username}. Input was: ${response}`
     );
   },
 };
