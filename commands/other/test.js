4b64839ee5087f790e8b604c4825df8b444d1109
flowz0 <billylflowers@gmail.com>
2025-01-24 18:49:42 -0800

    Created test command

---
+++ commands/other/test.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("test")
+        .setDescription("Test command")
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('The input to echo back')
+        ),
+    async execute(interaction) {
+        const response = interaction.options.getString('input');
+
+        await interaction.reply(
+            `[UPDATED] This is the test command ran by ${interaction.user.username}. Input was: ${response}`
+        );
+    },
+};
